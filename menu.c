444fac01050d8cd1f62bbe410d324ad54d3aba65
fnil <fatemeh@gmail.com>
2025-01-02 20:45:27 +0330

    completed 3 menus. "rogue login" should connect to the game. "before you play" has a sub menu. scorefor every username should be included in next changes. the score should be considered in the users.txt in its used in score board menu.

--- menu.c
+++ menu.c
@@ -11,18 +11,16 @@
 #define MAX_LENGTH 100
 // Menu choices
 char *choices[] = {
-        "Create New Rogue",
-        "Rogue Login",
-        "Before You Play",
-        "Score Board",
-        "Setting",
-        "Exit",
+        "1. Create New Rogue",
+        "2. Rogue Login",
+        "3. Before You Play",
+        "4. Exit",
         NULL,
 };
 
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
-void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color);
+void create_new_rogue(MENU *menu, WINDOW *menu_win);
 bool username_exists(const char *filename, const char *username);
 void register_user(const char *filename, const char *username, const char *password);
 bool contains_number(const char *password);
@@ -31,9 +29,10 @@ bool contains_uppercase(const char *password);
 bool is_valid_email(const char *email);
 char *generate_random_password();
 
+void login_rogue(MENU *menu, WINDOW *menu_win);
+bool validate_credentials(const char *filename, const char *username, const char *password);
 
-
-
+void before_you_play(MENU *menu, WINDOW *menu_win);
 
 int main() {
     ITEM **items;
@@ -100,11 +99,17 @@ int main() {
             case 10:  // Enter key
             {
                 ITEM *cur = current_item(menu);
-                if (strcmp(item_name(cur), "Exit") == 0) {
+                if (strcmp(item_name(cur), "4. Exit") == 0) {
                     goto end;  // Exit the menu loop
-                } else if (strcmp(item_name(cur), "Create New Rogue") == 0) {
-                    create_new_rogue(menu, menu_win, COLOR_PAIR(2));  // Pass the menu and menu_win
+                } else if (strcmp(item_name(cur), "1. Create New Rogue") == 0) {
+                    create_new_rogue(menu, menu_win);
+
+                } else if (strcmp(item_name(cur), "2. Rogue Login") == 0) {
+                    login_rogue(menu, menu_win);
+                } else if (strcmp(item_name(cur), "3. Before You Play") == 0) {
+                    before_you_play(menu, menu_win);
                 }
+
             }
                 break;
 
@@ -155,14 +160,14 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
     wrefresh(win);
 }
 
-// Function to handle the "Create New Rogue" option
-void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color) {
+// "Create New Rogue" option
+void create_new_rogue(MENU *menu, WINDOW *menu_win) {
     clear();
     WINDOW *form_win = newwin(14, 50, (LINES - 12) / 2, (COLS - 50) / 2);
     box(form_win, 0, 0);
-    wattron(form_win, COLOR_PAIR(2));
+    wattron(form_win, COLOR_PAIR(1));
     mvwprintw(form_win, 1, 16, "Create New Rogue");
-    wattroff(form_win, COLOR_PAIR(2));
+    wattroff(form_win, COLOR_PAIR(1));
     mvwhline(form_win, 2, 1, ACS_HLINE, 48);
     mvwprintw(form_win, 3, 2, "Enter Username:");
     mvwprintw(form_win, 5, 2, "Enter Password:");
@@ -187,9 +192,8 @@ void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color) {
             break;
         }
 
-    }
+    }   //validate username
     mvwprintw(form_win, 9, 3, "                                             ");
-
 
     while (1) {
         wattron(form_win, COLOR_PAIR(2));
@@ -218,12 +222,9 @@ void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color) {
                 wrefresh(form_win);
             }
         }
-    }
+    }   //validate password
     mvwprintw(form_win, 9, 3, "                                             ");
     mvwprintw(form_win, 10, 3, "                                             ");
-
-
-
 
     while (1) {     //check if email is valid
         mvwgetnstr(form_win, 7, 15, email, 29);
@@ -239,7 +240,7 @@ void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color) {
             mvwprintw(form_win, 7, 15, "             ");  // Clear the email input line
             wrefresh(form_win);
         }
-    }
+    }   //validate email
 
     noecho();
 
@@ -249,7 +250,8 @@ void create_new_rogue(MENU *menu, WINDOW *menu_win, chtype color) {
         wattron(form_win, COLOR_PAIR(1));
         mvwprintw(form_win, 9, 3, "Username already exists! Try again.");
         wattroff(form_win, COLOR_PAIR(1));
-    } else {
+    }
+    else {
         register_user(filename, username, password);
         wattron(form_win, COLOR_PAIR(2));
         mvwprintw(form_win, 9, 3, "User registered successfully!");
@@ -339,7 +341,6 @@ bool is_valid_email(const char *email) {
     }
     return false;
 }
-
 bool username_exists(const char *filename, const char *username) {
     FILE *file = fopen(filename, "r");
     if (!file) {
@@ -360,7 +361,6 @@ bool username_exists(const char *filename, const char *username) {
     fclose(file);
     return false;  // Username not found
 }
-
 void register_user(const char *filename, const char *username, const char *password) {
     if (username_exists(filename, username)) {
         printf("Username '%s' already exists. Please choose another.\n", username);
@@ -378,3 +378,186 @@ void register_user(const char *filename, const char *username, const char *passw
 //    printf("User '%s' registered successfully!\n", username);
 }
 
+
+// "Rogue Login" option
+void login_rogue(MENU *menu, WINDOW *menu_win) {
+    clear();
+    WINDOW *form_win = newwin(14, 50, (LINES - 12) / 2, (COLS - 50) / 2);
+    box(form_win, 0, 0);
+    wattron(form_win, COLOR_PAIR(1));
+    mvwprintw(form_win, 1, 16, "Rogue Login");
+    wattroff(form_win, COLOR_PAIR(1));
+
+    mvwhline(form_win, 2, 1, ACS_HLINE, 48);
+    mvwprintw(form_win, 3, 2, "Enter Username:");
+    mvwprintw(form_win, 5, 2, "Enter Password:");
+
+    wattron(form_win, COLOR_PAIR(2));
+    mvwprintw(form_win, 9, 2, "Enter Username or press 'G' for Guest Player");
+    wattroff(form_win, COLOR_PAIR(2));
+
+
+    char username[30], password[30];
+    echo();
+
+    mvwgetnstr(form_win, 3, 18, username, 29);
+
+    // Check if user wants to log in as Guest
+    if (strlen(username) == 1 && (username[0] == 'G' || username[0] == 'g')) {
+        mvwprintw(form_win, 9, 2, "                                             ");
+        wattron(form_win, COLOR_PAIR(2));
+        mvwprintw(form_win, 9, 3, "Logged in as Guest Player!");
+        wattroff(form_win, COLOR_PAIR(2));
+        wrefresh(form_win);
+        wgetch(form_win);
+        delwin(form_win);
+        clear();
+        refresh();
+        post_menu(menu);
+        wrefresh(menu_win);
+        return;
+    }
+
+    mvwgetnstr(form_win, 5, 18, password, 29);
+
+    noecho();
+
+    // Verify username and password
+    const char *filename = "users.txt";
+    if (validate_credentials(filename, username, password)) {
+        mvwprintw(form_win, 9, 2, "                                             ");
+        wattron(form_win, COLOR_PAIR(2));
+        mvwprintw(form_win, 9, 3, "Login successful!");
+        wattroff(form_win, COLOR_PAIR(2));
+    } else {
+        mvwprintw(form_win, 9, 2, "                                             ");
+        wattron(form_win, COLOR_PAIR(1));
+        mvwprintw(form_win, 9, 3, "Invalid username or password!");
+        wattroff(form_win, COLOR_PAIR(1));
+
+    }
+
+    wattron(form_win, COLOR_PAIR(2));
+    mvwprintw(form_win, 11, 3, "Press any key to return to the menu...");
+    wattroff(form_win, COLOR_PAIR(2));
+    wrefresh(form_win);
+    wgetch(form_win);
+
+    delwin(form_win);
+
+    // Clear and redisplay the menu
+    clear();
+    refresh();
+    post_menu(menu);
+    wrefresh(menu_win);
+
+
+}
+bool validate_credentials(const char *filename, const char *username, const char *password) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Could not open file");
+        return false;
+    }
+
+    char line[100];
+    while (fgets(line, sizeof(line), file)) {
+        char stored_username[30], stored_password[30];
+        sscanf(line, "%[^:]:%s", stored_username, stored_password);
+        if (strcmp(stored_username, username) == 0 && strcmp(stored_password, password) == 0) {
+            fclose(file);
+            return true;
+        }
+    }
+
+    fclose(file);
+    return false;
+}
+
+
+void before_you_play(MENU *menu, WINDOW *menu_win) {
+    // Submenu options
+    char *sub_choices[] = {
+            "1. Create New Game",
+            "2. Continue Last Game",
+            "3. Score Board",
+            "4. Setting",
+            "5. Profile",
+            "6. Return to Main Menu",
+            NULL
+    };
+
+    int n_sub_choices = ARRAY_SIZE(sub_choices) - 1;
+    ITEM **sub_items = (ITEM **)calloc(n_sub_choices + 1, sizeof(ITEM *));
+    for (int i = 0; i < n_sub_choices; ++i)
+        sub_items[i] = new_item(sub_choices[i], NULL);
+    sub_items[n_sub_choices] = NULL;
+
+    MENU *sub_menu = new_menu((ITEM **)sub_items);
+
+    // Create submenu window
+    WINDOW *sub_menu_win = newwin(12, 40, (LINES - 12) / 2, (COLS - 40) / 2);
+    keypad(sub_menu_win, TRUE);
+
+    set_menu_win(sub_menu, sub_menu_win);
+    set_menu_sub(sub_menu, derwin(sub_menu_win, 8, 38, 3, 1));
+    set_menu_mark(sub_menu, " > ");
+
+    // Print a border and title
+    box(sub_menu_win, 0, 0);
+    print_in_middle(sub_menu_win, 1, 0, 40, "Before You Play", COLOR_PAIR(1));
+    mvwaddch(sub_menu_win, 2, 0, ACS_LTEE);
+    mvwhline(sub_menu_win, 2, 1, ACS_HLINE, 38);
+    mvwaddch(sub_menu_win, 2, 39, ACS_RTEE);
+
+    // Post the menu
+    post_menu(sub_menu);
+    wrefresh(sub_menu_win);
+
+    int c;
+    while ((c = wgetch(sub_menu_win)) != 'q') {
+        switch (c) {
+            case KEY_DOWN:
+                menu_driver(sub_menu, REQ_DOWN_ITEM);
+                break;
+            case KEY_UP:
+                menu_driver(sub_menu, REQ_UP_ITEM);
+                break;
+            case 10: {  // Enter key
+                ITEM *cur = current_item(sub_menu);
+                const char *name = item_name(cur);
+
+                if (strcmp(name, "6. Return to Main Menu") == 0) {
+                    goto end;
+                } else if (strcmp(name,"1. Create New Game") == 0) {
+                    mvwprintw(sub_menu_win, 10, 2, "Create New Game is displayed here!");
+                } else if (strcmp(name, "2. Continue Last Game") == 0) {
+                    mvwprintw(sub_menu_win, 10, 2, "Continue Last Game is displayed here!");
+                } else if (strcmp(name, "3. Score Board") == 0) {
+                    mvwprintw(sub_menu_win, 10, 2, "Score Board is here!");
+                } else if (strcmp(name, "4. Setting") == 0) {
+                    mvwprintw(sub_menu_win, 10, 2, "Game Settings are here!");
+                } else if(strcmp(name, "5. Profile") == 0){
+                    mvwprintw(sub_menu_win, 10, 2, "Profile is here!");
+                }
+                wclrtoeol(sub_menu_win);
+                wrefresh(sub_menu_win);
+            }
+                break;
+        }
+    }
+
+    end:
+    // Cleanup
+    unpost_menu(sub_menu);
+    free_menu(sub_menu);
+    for (int i = 0; i < n_sub_choices; ++i)
+        free_item(sub_items[i]);
+    delwin(sub_menu_win);
+
+    // Return to main menu
+    clear();
+    refresh();
+    post_menu(menu);
+    wrefresh(menu_win);
+}
